6c6cf77057090cf89e90a5dce976eca637a0bb85
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2023-01-12 13:07:50 -0300

    feat: added the footer on the Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head';
-import styles from '../styles/home.module.scss'
+import styles from '../styles/home.module.scss';
+import Image from 'next/image';
+import techsImages from '../../public/images/techs.svg';
 
 export default function Home(): JSX.Element {
   return (
@@ -40,14 +42,28 @@ export default function Home(): JSX.Element {
         <hr className={styles.divisor} />
 
         <div className={styles.sectionContent}>
-          <img src="/images/webDev.png" alt="conteúdos desenvolvimento de aplicações web" />
+          <img
+            src="/images/webDev.png"
+            alt="conteúdos desenvolvimento de aplicações web"
+          />
           <section>
             <h2>Aprenda a criar Sistemas web</h2>
             <span>
-              Criar sistemas web, sites usando tecnologias mais modernas e requisitadas pelo mercado.
+              Criar sistemas web, sites usando tecnologias mais modernas e requisitadas
+              pelo mercado.
             </span>
           </section>
+        </div>
 
+        <div className={styles.nextLevelContent}>
+          <Image src={techsImages} alt="Tecnologias" />
+          <h2>
+            Mais de <span className={styles.alunos}>15 mil</span> já levaram sua carreira ao próximo nível.
+          </h2>
+          <span>E você vai perder a chance de evoluir de uma vez por todas?</span>
+          <a href=''>
+            <button>ACESSAR TURMA</button>
+          </a>
         </div>
       </main>
     </>
