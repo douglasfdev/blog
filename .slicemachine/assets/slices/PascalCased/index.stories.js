16e755df557a1552c046ac8ef09c2acecd99d3d1
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2023-01-12 22:29:46 -0300

    feat: add Prismic.io to projet and configure it

---
+++ .slicemachine/assets/slices/PascalCased/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/PascalCased';
+
+export default {
+  title: 'slices/PascalCased'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"heading1","text":"Article","spans":[]}],"description":[{"type":"paragraph","text":"Eu id et et ea est. Fugiat laborum ea deserunt ea amet fugiat quis aliqua mollit. Incididunt incididunt eu velit.","spans":[]}]},"id":"_Default","slice_type":"pascal_cased"}} />
+_Default.storyName = ''
